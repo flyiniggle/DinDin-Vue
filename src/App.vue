ac3c2c4b42ff73cc844ccbf3066b930a3bccfc6c
Dan Thompson <dthompson@vestmark.com>
2018-01-17 07:58:22 -0500

    a header, bootstraps, and some less

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,26 @@
+
+
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <toolbar></toolbar>
     <router-view/>
   </div>
 </template>
 
 <script>
+  import Toolbar from "@/components/Toolbar.vue"
+
 export default {
+  	components: {
+  		toolbar: Toolbar
+    },
   name: 'App'
 }
 </script>
 
-<style>
+<style lang="less">
+  @import '../node_modules/bootstrap/less/bootstrap.less';
+  @import './less/variables.less';
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Toolbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="toolbar">
+    <img id="logo" src="@/assets/Din-Din Logo.png">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Toolbar'
+  }
+</script>
+
+<style lang="less">
+  @import '../../node_modules/bootstrap/less/bootstrap.less';
+  @import '../less/variables.less';
+  #toolbar {
+    width: 100%;
+    height: 80px;
+    background-color: @brand-brown;
+  }
+
+  #logo {
+    display: inline-block;
+    float: left;
+  }
+</style>
